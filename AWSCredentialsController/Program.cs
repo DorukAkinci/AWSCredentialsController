d8805883712d336f8a398588b6ac2f62a63d394b
DORUK AKINCI (IT) <doruk.akinci@digiturk.com.tr>
2018-05-02 10:59:37 +0300

    Add project files.

---
+++ AWSCredentialsController/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NDesk.Options;
+using ConsoleTables;
+using Console = Colorful.Console;
+using System.Drawing;
+using Amazon.Runtime.CredentialManagement;
+
+namespace AWSCredentialsController
+{
+    class Program
+    {
+        static string _newProfile = "";
+        static string _profile = "";
+        static string _accessKey = "";
+        static string _secretKey = "";
+        static bool _help = false;
+        static bool _list = false;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                OptionSet _optionSet = new OptionSet(){
+                    { "l|list", "Active profilleri listelemeyi sağlar." , q => _list = q != null},
+                    { "np|newprofile=", "Yeni credential tanımı için profil adı belirlemenizi sağlar.", q => _newProfile = q },
+                    { "p|profile=", "Amazon ortamına bağlanmak için hazırda profile tanımı varsa kullanabilmeyi sağlar.", q => _profile = q },
+                    { "ak|accesskey=", "Amazon ortamına bağlanmak için kullanılacak access key'i tanımlar.", q => _accessKey = q },
+                    { "sk|secretkey=", "Amazon ortamına bağlanmak için kullanılacak secret key'i tanımlar.", q => _secretKey = q },
+                    { "h|?|help", "Bilgi almak için kullanılabilir." , q => _help = q != null}
+                };
+
+                var container = _optionSet.Parse(args);
+                if ((_help) || (args.Length == 0))
+                {
+                    Console.WriteLine("Program için kullanılabilinecek komutlar:");
+                    _optionSet.WriteOptionDescriptions(Console.Out);
+                }
+                else
+                    ExecuteTheParameters();
+
+                Console.WriteLine("Program tamamlandı.");
+            }
+            catch (OptionException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("You can use --help for instructions");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unexpected ERROR: " + ex.Message);
+            }
+#if DEBUG
+            finally
+            {
+                Console.ReadKey();
+            }
+#endif
+        }
+
+        private static void ExecuteTheParameters()
+        {
+            if (_list) ListCredentialProfiles();
+        }
+
+        private static void ListCredentialProfiles()
+        {
+            ConsoleOutputMakeStars();
+            Console.WriteLine("Listing all existing profiles:", Color.Red);
+            var _table = new ConsoleTable("Profile", "Type", "AccessKey", "SecretKey");
+            SharedCredentialsFile _sharedCredentialsFile = new SharedCredentialsFile();
+            var _profileList = _sharedCredentialsFile.ListProfiles();
+            foreach (var _profile in _profileList)
+            {
+                _table.AddRow(_profile.Name, _profile.CredentialDescription, _profile.Options.AccessKey, _profile.Options.SecretKey);
+            }
+            _table.Write();
+        }
+
+        private static void ConsoleOutputMakeStars()
+        {
+            Console.WriteLine("******************");
+        }
+    }
+}
---
+++ AWSCredentialsController/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AWSCredentialsController")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AWSCredentialsController")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0995f28e-afc8-4b3b-98d6-26d56f1c28a8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
